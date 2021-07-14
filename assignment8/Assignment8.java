ffec1a98426e6731aa87bad0309a800e68b2928b
Francis Cruz <jeni13franc@gmail.com>
2021-07-14 07:09:02 +0530

    Made changes

--- assignment8/Assignment8.java
+++ assignment8/Assignment8.java
@@ -1,25 +1,18 @@
-/*
- * Given two strings, word and a separator, return a big string made of count 
- * occurrences of the word, separated by the separator string.
- * if the inputs are "Wipro","X" and 3 then the output is "WiproXWiproXWipro".
- * */
-
-package com.w3epic.wiprotraining.assignment8;
 
 public class Assignment8 {
 
 	public static void main(String[] args) {
-		String str1 = "Wipro";
-		String str2 = "X";
-		int separator = 3;
+		String str = "ab*cd";
+		
+		String[] strs = str.split(".[\\*]+.");
 		
 		StringBuffer sb = new StringBuffer();
 		
-		for (int i = 0 ; i < separator - 1; i++) 
-			sb.append(str1).append(str2);
-		sb.append(str1);
+		for (String x : strs)
+			sb.append(x);
 		
 		System.out.println(sb);
+
 	}
 
 }
